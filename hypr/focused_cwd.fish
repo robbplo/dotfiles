cf7972f163c2076b3c008bcf753e9141ec032dda
Robbin Ploeger <robbplo@gmail.com>
2024-05-19 08:29:02 +0200

    (hyprland): add config

---
+++ hypr/focused_cwd.fish
@@ -0,0 +1,9 @@
+#!/bin/fish
+# Get the cwd of the currently focused terminal window
+
+pid=(hyprctl activewindow | jq '.pid')
+ppid=(pgrep --newest --parent "$pid")
+cwd=(readlink /proc/"$ppid"/cwd || echo "$HOME")
+
+[ -d "$cwd" ] && echo "$cwd" || echo "$HOME"
+
