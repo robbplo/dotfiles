c286aab2604d9573e2280e12a000c58c23b0fe5a
Robbin Ploeger <robbplo@gmail.com>
2024-06-01 09:50:00 +0200

    (nvim): add fzf extension for telescope

--- nvim/lua/packer_init.lua
+++ nvim/lua/packer_init.lua
@@ -18,6 +18,8 @@ return require('packer').startup(function(use)
 
   -- File navigation
   use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
   use "stevearc/oil.nvim"
 
   -- QFlist and diagnostics list
--- nvim/lua/plugins/nvim-telescope.lua
+++ nvim/lua/plugins/nvim-telescope.lua
@@ -1,16 +1,14 @@
-local trouble = require("trouble.providers.telescope")
+local trouble = require("trouble.sources.telescope")
 
 local telescope = require("telescope")
 
 telescope.setup {
   defaults = {
     mappings = {
-      i = { ["<c-t>"] = trouble.open_with_trouble },
-      n = { ["<c-t>"] = trouble.open_with_trouble },
-    },
-    file_ignore_patterns = {
-      "public",
-      "storage",
+      i = { ["<c-t>"] = trouble.open },
+      n = { ["<c-t>"] = trouble.open },
     },
   },
 }
+
+require('telescope').load_extension('fzf')
